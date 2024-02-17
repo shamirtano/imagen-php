f8cfbf81350f3767619ccf7b5f2cd8a84da02c39
Shamir Tano Morales <shamirtano@gmail.com>
2024-02-17 17:58:29 -0500

    first commit :tada:

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Autoloader
+require_once '../vendor/autoload.php';
+
+// Load Config
+require_once '../config/config.php';
+require_once '../config/database.php';
+
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
